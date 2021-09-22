2c5478fd01bb8922e57e74f5ebdaa7c014535b4e
dinaamir <dinamarini00@gmail.com>
2021-09-22 12:18:33 +0800

    commit

---
+++ testing.py
@@ -0,0 +1,18 @@
+import streamlit as st 
+import numpy as np 
+import pandas as pd
+import altair as alt
+from streamlit_folium import folium_static
+import folium
+
+st.title("hello world :)")
+st.header("this is the head")
+st.subheader("this is the neck") #subheader
+#image
+st.text("this is the body")
+
+
+st.markdown('# kill me')
+st.markdown('## kill me')
+st.markdown('** kill me **')
+st.markdown("* kill me")
